7f37330b295a6951b6b1d3bfbb75dee61d1f482e
yraffic02 <yraffic02@gmail.com>
2024-03-04 15:37:07 -0300

    chore: texto alterado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="font-workSans font-title text-title text-verdeFloresta">
-      work sans
+      Criar Onboarding aqui e no final direcionar para rota /login
     </h1>
   );
 }
