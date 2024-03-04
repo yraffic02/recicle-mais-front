2e53877e72d616640921f528d564d528c6ee46a4
yraffic02 <yraffic02@gmail.com>
2024-03-04 15:32:09 -0300

    feat: adicionado espaçamento para toda a aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <div className="p-6">
+          {children}  
+        </div>
+      </body>
     </html>
   );
 }
