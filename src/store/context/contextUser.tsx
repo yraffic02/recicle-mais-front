aa0695a3b5d89f4f654ba47a6ec5562c92191395
yraffic02 <yraffic02@gmail.com>
2024-03-04 15:01:48 -0300

    feat: criado contexto usuario

---
+++ src/store/context/contextUser.tsx
@@ -0,0 +1,27 @@
+"use client";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+} from "react";
+
+interface IUserProps {
+  children: ReactNode;
+}
+
+interface IUserContext {}
+
+export const UserContext = createContext<IUserContext>({} as IUserContext);
+
+export function UserProvider({ children }: IUserProps) {
+
+  const valuesContext: IUserContext = {};
+
+  return (
+    <UserContext.Provider value={valuesContext}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export const useMenuContext = () => useContext(UserContext);
