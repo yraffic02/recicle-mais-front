76710acd0859f4eb0d3025b4fcfe38180d07ef00
yraffic02 <yraffic02@gmail.com>
2024-03-07 17:13:23 -0300

    feat: criado Componete button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import { ButtonHTMLAttributes, ReactNode } from "react"
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: ReactNode
+    typeButton?: string
+}
+
+export const Button = ({children, typeButton = 'primary', ...props}: ButtonProps) =>{
+    
+    if(typeButton === "secondary"){
+        return(
+            <button
+                {...props}
+                className="w-full p-3 rounded-[2rem] hover:opacity-85 bg-cinzaClaroSec text-cinzaClaro font-medium text-base"
+            >
+                {children}
+            </button>
+        )
+    }
+
+    return(
+        <button
+            {...props}
+            className="w-full p-3 rounded-[2rem] hover:opacity-85 bg-verdeFloresta text-white font-medium text-base"
+        >
+            {children}
+        </button>
+    )
+}
