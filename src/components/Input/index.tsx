300775a1a0c45a130f5bae5478c3d56c826b665c
yraffic02 <yraffic02@gmail.com>
2024-03-07 19:35:32 -0300

    feat: criado componente input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,28 @@
+import {
+  InputHTMLAttributes,
+  forwardRef
+} from "react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ name, ...rest }, ref) => {
+    return (
+      <input
+        {...rest}
+        name={name}
+        ref={ref}
+        className="
+          border-cinzaClaroSec 
+          text-cinzaEscuro 
+          border-[0.06rem] 
+          rounded-[2rem] 
+          p-3 
+          w-full
+          focus:placeholder-black"
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
