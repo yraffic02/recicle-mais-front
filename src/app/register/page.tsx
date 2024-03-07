00108a32bbbd697b3fc388492333b88a20d00aea
yraffic02 <yraffic02@gmail.com>
2024-03-07 17:13:54 -0300

    chore: Button add

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/Button";
 import { Header } from "@/components/Header";
+import Image from "next/image";
 
 export default function PageRegister(){
     return(
@@ -7,6 +9,31 @@ export default function PageRegister(){
                 <h1>Cadastro</h1>
                 <div></div>
             </Header>
+
+            <div className="flex flex-col items-center justify-center gap-4">
+                <Image 
+                    src='/logo/Logo.svg'
+                    height={24}
+                    width={115}
+                    alt="logo recicle mais"
+                />
+                <h2 className="font-workSans font-semibold text-cinzaEscuro">
+                    Faça parte deste movimento, 
+                    e ajude o mundo a se tornar cada vez mais limpo 
+                    e sustentável!
+                </h2>
+                <Button
+                    typeButton="secondary"
+                >
+                    Cadastrar com Google
+                </Button>
+                <Button
+                    typeButton="secondary"
+                >
+                    Cadastrar com Facebook
+                </Button>
+            </div>
+                
         </main>
     )
 }
