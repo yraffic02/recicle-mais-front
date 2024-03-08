ad0469b59a9272c6da9cc9a759606796a5c68f22
yraffic02 <yraffic02@gmail.com>
2024-03-08 14:18:17 -0300

    chore: ajuste na altura da page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,10 @@ import { FormRegister } from "./components/FormRegister";
 
 export default function PageRegister(){
     return(
-        <main className="flex flex-col justify-between m11 h-full relative">
+        <main 
+            className="flex flex-col justify-between m11 h-full relative" 
+            style={{ height: "calc(100vh - 5.5rem)" }}
+        >
             <FormRegister />
         </main>
     )
