c41f04b689649775fd43d08c8161900912ea0129
yraffic02 <yraffic02@gmail.com>
2024-03-08 11:58:31 -0300

    chore: adicionado form register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,9 @@
+import { FormRegister } from "./components/FormRegister";
 
 export default function PageRegister(){
     return(
         <main className="flex flex-col justify-between m11 h-full relative">
-            
+            <FormRegister />
         </main>
     )
 }
