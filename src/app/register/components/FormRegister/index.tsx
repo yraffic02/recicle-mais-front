00ca9b859565e7f3637db4f301be7a84ea8f6e49
yraffic02 <yraffic02@gmail.com>
2024-03-08 14:17:55 -0300

    chore: criando estado de formulario

--- src/app/register/components/FormRegister/index.tsx
+++ src/app/register/components/FormRegister/index.tsx
@@ -1,10 +1,18 @@
+'use client'
 import { Button } from "@/components/Button"
 import { Input } from "@/components/Input"
 import { Label } from "@/components/Label"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 export const FormRegister = () =>{
+    const [formState, setFormState] = useState(false)
+
+    const handleFormState = () =>{
+        setFormState(!formState)
+    }
+
     return(
         <div className="media-md-h">
             <section className="flex flex-col items-center justify-center gap-4">
@@ -15,44 +23,66 @@ export const FormRegister = () =>{
                     alt="logo recicle mais"
                 />
 
-                <h2 className="font-semibold text-cinzaEscuro">
-                    Faça parte deste movimento, 
-                    e ajude o mundo a se tornar cada vez mais limpo 
-                    e sustentável!
-                </h2>
+                {
+                    formState &&
+                    <h2 className="font-semibold text-cinzaEscuro">
+                       Quase lá! Escolha um avatar que melhor 
+                       te representa para continuar seu cadastro
+                    </h2>
+                }
 
-                <Button
-                    typeButton="secondary"
-                >
-                    <Image 
-                        src='/icons/iconeGoogle.svg'
-                        height={24}
-                        width={24}
-                        alt="icone Google"
-                    />
-                    <p>Cadastrar com Google</p>
-                </Button>
+                {
+                    !formState &&
+                    <h2 className="font-semibold text-cinzaEscuro">
+                        Faça parte deste movimento, 
+                        e ajude o mundo a se tornar cada vez mais limpo 
+                        e sustentável!
+                    </h2>
+                }
 
-                <Button
-                    typeButton="secondary"
-                >   
-                    <Image 
-                        src='/icons/iconeFacebook.svg'
-                        height={24}
-                        width={24}
-                        alt="icone Google"
-                    />
-                    <p>Cadastrar com Facebook</p>
-                </Button>
+                {
+                    !formState &&
+                    <>
+                        <Button
+                            typeButton="secondary"
+                        >
+                            <Image 
+                                src='/icons/iconeGoogle.svg'
+                                height={24}
+                                width={24}
+                                alt="icone Google"
+                            />
+                            <p>Cadastrar com Google</p>
+                        </Button>
+
+                        <Button
+                            typeButton="secondary"
+                        >   
+                            <Image 
+                                src='/icons/iconeFacebook.svg'
+                                height={24}
+                                width={24}
+                                alt="icone Google"
+                            />
+                            <p>Cadastrar com Facebook</p>
+                        </Button>
+                    </>
+                }
             </section>
-            <form>
+
+            {
+                !formState && 
                 <div className="w-full pt-5">
                     <h1 className="font-semibold text-cinzaEscuro text-center">
                         Ou
                     </h1>
                 </div> 
+            }
 
-                <section className="flex flex-col items-start gap-2 mb-3 pb-20">
+            <form>
+                {
+                    !formState &&
+                    <section className="flex flex-col items-start gap-2 mb-3 pb-20">
                 
                         <Label labelHtmlFor="name">Nome</Label>
                         <Input id="name" placeholder="Digite aqui o seu nome" />
@@ -65,11 +95,44 @@ export const FormRegister = () =>{
 
                         <Label labelHtmlFor="confirm">Confirmar senha</Label>
                         <Input id="confirm" placeholder="Digite novamente sua senha" />
-                </section>
+                    </section>
+                }
+
+                {
+                    formState && 
+                    <h1>avatares</h1>
+                }
+
+                {
+                    formState &&
+                    <div className="flex flex-col items-center justify-center gap-2  w-full absolute bottom-0">
+                        <Button
+                            typeButton="primary"
+                        >
+                            Criar Conta
+                        </Button>
+                        <div className="flex items-center justify-center gap-1">
+                            <input 
+                                type="checkbox" 
+                                id="terms" 
+                                className="h-5 w-5 rounded" 
+                            />
+                            <label 
+                                htmlFor="terms"
+                                className="font-semibold text-sm"
+                            >
+                                Aceito os termos e políticas de privacidade
+                            </label>
+                        </div>  
+                    </div> 
+                }
             </form>
+           {
+            !formState &&
             <div className="flex flex-col items-center justify-center gap-1 bg-white w-full absolute bottom-0">
                 <Button
                     typeButton="primary"
+                    onClick={handleFormState}
                 >
                     Continuar
                 </Button>
@@ -82,7 +145,8 @@ export const FormRegister = () =>{
                         Entrar
                     </Link>
                 </div>
-            </div>
+            </div> 
+           }
         </div>
     )
 }
