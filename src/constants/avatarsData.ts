f0e453da514c0bb6d6276319bf00c2391c62d4cc
yraffic02 <yraffic02@gmail.com>
2024-03-08 14:28:40 -0300

    feat: criado constantes de avatares

---
+++ src/constants/avatarsData.ts
@@ -0,0 +1,13 @@
+export const dataAvatars: string[] = [
+    'avatar-profile01.svg',
+    'avatar-profile02.svg',
+    'avatar-profile03.svg',
+    'avatar-profile04.svg',
+    'avatar-profile05.svg',
+    'avatar-profile06.svg',
+    'avatar-profile07.svg',
+    'avatar-profile08.svg',
+    'avatar-profile09.svg',
+    'avatar-profile10.svg',
+    'avatar-profile11.svg',
+]
