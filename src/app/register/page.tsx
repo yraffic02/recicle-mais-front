78048aa4eebcc064de925366fb13d4ca86b5a5a7
yraffic02 <yraffic02@gmail.com>
2024-03-08 11:46:48 -0300

    chore: ajuste no form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,82 +6,86 @@ import Link from "next/link";
 
 export default function PageRegister(){
     return(
-        <main className="flex flex-col justify-between h-[570px] overflow-auto">
-            <section className="flex flex-col items-center justify-center gap-4">
+        <main className="flex flex-col justify-between m11 h-full relative">
 
-                <Image 
-                    src='/logo/Logo.svg'
-                    height={24}
-                    width={115}
-                    alt="logo recicle mais"
-                />
+            <form className="media-md-h">
+                <section className="flex flex-col items-center justify-center gap-4">
 
-                <h2 className="font-workSans font-semibold text-cinzaEscuro">
-                    Faça parte deste movimento, 
-                    e ajude o mundo a se tornar cada vez mais limpo 
-                    e sustentável!
-                </h2>
-
-                <Button
-                    typeButton="secondary"
-                >
                     <Image 
-                        src='/icons/iconeGoogle.svg'
+                        src='/logo/Logo.svg'
                         height={24}
-                        width={24}
-                        alt="icone Google"
+                        width={115}
+                        alt="logo recicle mais"
                     />
-                    <p>Cadastrar com Google</p>
-                </Button>
 
-                <Button
-                    typeButton="secondary"
-                >   
-                    <Image 
-                        src='/icons/iconeFacebook.svg'
-                        height={24}
-                        width={24}
-                        alt="icone Google"
-                    />
-                    <p>Cadastrar com Facebook</p>
-                </Button>
-            </section>
+                    <h2 className="font-workSans font-semibold text-cinzaEscuro">
+                        Faça parte deste movimento, 
+                        e ajude o mundo a se tornar cada vez mais limpo 
+                        e sustentável!
+                    </h2>
 
-            <div className="w-full pt-5">
-                <h1 className="font-semibold text-cinzaEscuro text-center">
-                    Ou
-                </h1>
-            </div> 
-
-            <section>
-                <form className="flex flex-col items-start gap-2 mb-3">
-                    <Label labelHtmlFor="name">Nome</Label>
-                    <Input id="name" placeholder="Digite aqui o seu nome" />
-
-                    <Label labelHtmlFor="email">Entrar com outro e-mail</Label>
-                    <Input id="email" placeholder="Digite seu email" />
-
-                    <Label labelHtmlFor="password">Senha</Label>
-                    <Input id="password" placeholder="Digite uma senha" />
-
-                    <Label labelHtmlFor="confirm">Confirmar senha</Label>
-                    <Input id="confirm" placeholder="Digite novamente sua senha" />
-
-                </form>
-                <Button
-                    typeButton="primary"
-                >
-                    Continuar
-                </Button>
-                <div className="flex items-center justify-center">
-                    <p>Já possui uma conta?</p>
-                    <Link 
-                        href='/login'
+                    <Button
+                        typeButton="secondary"
                     >
-                        Entrar
-                    </Link>
+                        <Image 
+                            src='/icons/iconeGoogle.svg'
+                            height={24}
+                            width={24}
+                            alt="icone Google"
+                        />
+                        <p>Cadastrar com Google</p>
+                    </Button>
+
+                    <Button
+                        typeButton="secondary"
+                    >   
+                        <Image 
+                            src='/icons/iconeFacebook.svg'
+                            height={24}
+                            width={24}
+                            alt="icone Google"
+                        />
+                        <p>Cadastrar com Facebook</p>
+                    </Button>
+                </section>
+
+                <div className="w-full pt-5">
+                    <h1 className="font-semibold text-cinzaEscuro text-center">
+                        Ou
+                    </h1>
+                </div> 
+
+                <section className="flex flex-col items-start gap-2 mb-3 pb-20">
+                
+                        <Label labelHtmlFor="name">Nome</Label>
+                        <Input id="name" placeholder="Digite aqui o seu nome" />
+
+                        <Label labelHtmlFor="email">Entrar com outro e-mail</Label>
+                        <Input id="email" placeholder="Digite seu email" />
+
+                        <Label labelHtmlFor="password">Senha</Label>
+                        <Input id="password" placeholder="Digite uma senha" />
+
+                        <Label labelHtmlFor="confirm">Confirmar senha</Label>
+                        <Input id="confirm" placeholder="Digite novamente sua senha" />
+                </section>
+                <div className="flex flex-col items-center justify-center gap-1 w-full bg-white absolute bottom-0">
+                    <Button
+                        typeButton="primary"
+                    >
+                        Continuar
+                    </Button>
+                    <div className="flex items-center justify-center">
+                        <p>Já possui uma conta?</p>
+                        <Link 
+                            href='/login'
+                        >
+                            Entrar
+                        </Link>
+                    </div>
                 </div>
-            </section>
+            </form>
+    
         </main>
     )
 }
