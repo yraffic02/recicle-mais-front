4a41bf5b7900097840fa4f7a322ac42529b9b691
yraffic02 <yraffic02@gmail.com>
2024-03-14 12:56:24 -0300

    chore: adicionado header

--- src/app/(routesApp)/profile/page.tsx
+++ src/app/(routesApp)/profile/page.tsx
@@ -1,7 +1,18 @@
+import { Header } from "@/components/Header";
+import { SecondaryActionButton } from "@/components/Header/components/ButtonActionSecondary";
+
 export default function ProfilePage(){
     return(
-        <main>
-            <h1>page profile</h1>
-        </main>
+        <>  
+            <Header>
+                <h1>Perfil</h1>
+                <SecondaryActionButton 
+                    srcIcon="/icons/notification.svg"
+                />
+            </Header>
+            <main className="h-full">
+                <h1>page profile</h1>
+            </main>
+        </>
     )
 }
