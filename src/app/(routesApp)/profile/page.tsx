000be421d7e16a998bf69c872bfcbe2d2a1fc25f
yraffic02 <yraffic02@gmail.com>
2024-03-14 16:43:55 -0300

    feat: criado header profile

--- src/app/(routesApp)/profile/page.tsx
+++ src/app/(routesApp)/profile/page.tsx
@@ -1,5 +1,8 @@
+import { Avatar } from "@/components/Avatar";
 import { Header } from "@/components/Header";
 import { SecondaryActionButton } from "@/components/Header/components/ButtonActionSecondary";
+import Image from "next/image";
+import { HeaderProfile } from "./components/HeaderProfile";
 
 export default function ProfilePage(){
     return(
@@ -10,8 +13,37 @@ export default function ProfilePage(){
                     srcIcon="/icons/notification.svg"
                 />
             </Header>
-            <main className="h-full">
-                <h1>page profile</h1>
+            <main className="h-full w-full py-6">
+                <section>
+                    <HeaderProfile />
+                </section>
+                <section className="w-full flex flex-col items-start">
+
+                    <h1 className="text-xl font-semibold">
+                        Configurações
+                    </h1>
+
+                    <ul className="w-full">
+                        <li className="w-full flex items-center justify-between">
+                            <div className="flex items-center gap-2">
+                                <Image 
+                                    src='/icons/profile.svg'
+                                    height={24}
+                                    width={24}
+                                    alt="profile"
+                                />
+                                <p>Minha Conta</p>
+                            </div>
+                            <Image 
+                                src='/icons/arrowRight.svg'
+                                height={24}
+                                width={24}
+                                alt="profile"
+                            />
+                        </li>
+                    </ul>
+
+                </section>
             </main>
         </>
     )
