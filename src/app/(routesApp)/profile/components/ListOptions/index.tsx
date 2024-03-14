4d44707e4f03fa0215b5d9626c7ba43d903569f8
yraffic02 <yraffic02@gmail.com>
2024-03-14 17:03:04 -0300

    chore: lista criada

--- src/app/(routesApp)/profile/components/ListOptions/index.tsx
+++ src/app/(routesApp)/profile/components/ListOptions/index.tsx
@@ -3,7 +3,26 @@ import { Options } from "./components/Options"
 export const ListOptions = () =>{
     return(
         <ul className="w-full">
-            <Options />
+            <Options 
+                icon="profile"
+                name="Minha Conta"
+            />
+            <Options 
+                icon="notificationFilled"
+                name="Notificações"
+            />
+            <Options 
+                icon="security"
+                name="Segurança e Privacidade"
+            />
+            <Options 
+                icon="termsFilled"
+                name="Termos de uso"
+            />
+            <Options 
+                icon="logout"
+                name="Sair"
+            />
         </ul>
     )
 }
