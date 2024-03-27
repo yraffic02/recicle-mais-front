0e6c18b794059e686cd423c294afb8a06e2c5fc2
yraffic02 <yraffic02@gmail.com>
2024-03-27 15:45:35 -0300

    chore: emovido espaçamento no header profile

--- src/app/(routesApp)/edite/page.tsx
+++ src/app/(routesApp)/edite/page.tsx
@@ -1,58 +1,34 @@
 import { Buttonsm } from "@/app/opening/components/buttonsm";
+import { Header } from "@/components/Header";
+import { SecondaryHeaderButton } from "@/components/Header/components/ButtonActionSecondary";
+import { Input } from "@/components/Input";
+import { Label } from "@/components/Label";
 import Image from "next/image";
 import Link from "next/link";
-import Header from "./components/header";
-import { InputEdite } from "./components/inputEdite";
+import { HeaderProfile } from "../profile/components/HeaderProfile";
 
 export default function EditePage() {
   return (
-    <div className="w-full h-screen p-6 flex flex-col justify-center items-center">
-      <Header title="Minha Conta" />
+    <div className="w-full h-screen flex flex-col">
+      <Header>
+          <h1 className="font-medium text-base">Minha Conta</h1>
+          <SecondaryHeaderButton 
+              srcIcon="/icons/notification.svg"
+          />
+      </Header>
       <div
-        className="w-full h-full flex justify-center items-center flex-col "
+        className="w-full h-full flex  items-center flex-col pt-7"
       >
-        <div className="flex justify-center items-center gap-6 w-full  mb-6">
-          <div className="w-24 h-24 flex justify-center items-center rounded-full">
-            <Image src="/avatar/avatar-profile01.svg" alt="logo" width={100} height={100} />
-          </div>
-          <div className="w-full flex justify-start items-center ">
-            <h1 className="font-workSans font-title text-xl leading-6">
-              Amanda Campos
-            </h1>
-          </div>
+        <div className="flex justify-center items-center w-full">
+          <HeaderProfile />
         </div>
 
-        <div className="w-full flex justify-center items-center gap-3 flex-col mb-6">
-          <InputEdite
-            label="Nome"
-            name="nome"
-            placeholder="Amanda Campos"
-            type="text"
-          />
-          <InputEdite
-            label="E-mail"
-            name="email"
-            placeholder="amandacampos274@gmail.com"
-            type="text"
-          />
-          <InputEdite
-            label="Número de celular"
-            name="telefone"
-            placeholder="32 923334-55555"
-            type="number"
-          />
-          <InputEdite
-            label="Endereço"
-            name="endereco"
-            placeholder="Rua das Flores, 123"
-            type="text"
-          />
-          <InputEdite
-            label="Senha"
-            name="password"
-            placeholder="*********"
-            type="password"
-          />
+        <div className="w-full flex justify-center  gap-3 flex-col mb-6">
+            <Label labelHtmlFor="email">Email</Label>
+            <Input id="email" placeholder="Digite aqui o seu Email" />
+
+            <Label labelHtmlFor="password">Senha</Label>
+            <Input id="password" placeholder="Digite sua senha" />
         </div>
 
         <div className="w-full flex justify-center items-center gap-1 mb-10">
