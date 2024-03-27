07974b8b9f072b1c6d4f7da070e87567a1007109
yraffic02 <yraffic02@gmail.com>
2024-03-27 16:02:29 -0300

    refactor: ajuste na tela editei

--- src/app/(routesApp)/edite/page.tsx
+++ src/app/(routesApp)/edite/page.tsx
@@ -9,36 +9,49 @@ import { HeaderProfile } from "../profile/components/HeaderProfile";
 
 export default function EditePage() {
   return (
-    <div className="w-full h-screen flex flex-col">
+    <div className="w-full h-full flex flex-col">
       <Header>
           <h1 className="font-medium text-base">Minha Conta</h1>
           <SecondaryHeaderButton 
               srcIcon="/icons/notification.svg"
           />
       </Header>
-      <div
-        className="w-full h-full flex  items-center flex-col pt-7"
-      >
-        <div className="flex justify-center items-center w-full">
+      <div className="w-full pb-6">
           <HeaderProfile />
-        </div>
+      </div>
+      <div
+        className="w-full h-full flex flex-col pt-7 media-md-h"
+      >
+        <form className="w-full flex justify-center  gap-3 flex-col mb-6">
+          <Label labelHtmlFor="email">Email</Label>
+          <Input id="email" placeholder="Digite aqui o seu Email" />
 
-        <div className="w-full flex justify-center  gap-3 flex-col mb-6">
-            <Label labelHtmlFor="email">Email</Label>
-            <Input id="email" placeholder="Digite aqui o seu Email" />
+          <Label labelHtmlFor="password">Senha</Label>
+          <Input id="password" placeholder="Digite sua senha" />
 
-            <Label labelHtmlFor="password">Senha</Label>
-            <Input id="password" placeholder="Digite sua senha" />
-        </div>
+          <Label labelHtmlFor="password">Senha</Label>
+          <Input id="password" placeholder="Digite sua senha" />
 
-        <div className="w-full flex justify-center items-center gap-1 mb-10">
-          <span className="font-workSans font-title text-body">Deseja alterar sua senha?</span>
-          <Link href={"/edite/editePassword"} ><strong className="font-workSans font-title text-body text-verdeFloresta"> Clique aqui!</strong></Link>
-        </div>
-        <Buttonsm color="verdeFloresta" size={"full"}>
-          <Buttonsm.Text color="white">Salvar</Buttonsm.Text>
-        </Buttonsm>
+          <Label labelHtmlFor="password">Senha</Label>
+          <Input id="password" placeholder="Digite sua senha" />
 
+          <Label labelHtmlFor="password">Senha</Label>
+          <Input id="password" placeholder="Digite sua senha" />
+          <div className="w-full flex justify-center items-center gap-1 mb-10">
+            <span className="font-workSans font-title text-body">
+              Deseja alterar sua senha?
+            </span>
+            <Link 
+              href="/edite/editePassword" 
+              className="font-title text-body text-verdeFloresta underline"
+            >
+              Clique aqui!
+            </Link>
+          </div>
+          <Buttonsm color="verdeFloresta" size={"full"}>
+            <Buttonsm.Text color="white">Salvar</Buttonsm.Text>
+          </Buttonsm>
+        </form>
       </div>
     </div>
   )
