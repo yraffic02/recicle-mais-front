6de67f387e8873661de0c93d4e9c2f9ec79dff04
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-03-28 16:35:42 -0300

    feat: formulário da página address do coletador funcionando

--- src/app/(routesApp)/how-to-recycle/collect-point/adress/page.tsx
+++ src/app/(routesApp)/how-to-recycle/collect-point/adress/page.tsx
@@ -1,10 +1,29 @@
+"use client";
 import { Avatar } from "@/components/Avatar";
 import { Button } from "@/components/Button";
 import { Header } from "@/components/Header/index";
 import { Input } from "@/components/Input/index";
 import { Label } from "@/components/Label";
+import { ChangeEvent, useState } from "react";
 
 export default function Adress() {
+  const [formValues, setFormValues] = useState({
+    cep: "",
+    endereco: "",
+    municipio: "",
+    estado: "",
+  });
+
+  const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log("Formulário enviado:", formValues);
+  };
+  
+console.log(formValues, "FORM")
   return (
     <>
       <div className="h-screen flex flex-col justify-between w-full">
@@ -12,20 +31,43 @@ export default function Adress() {
           <h1 className="text-base font-medium">Reciclar</h1>
           <Avatar />
         </Header>
-        <main style={{ height: "calc(100vh - 200px)" }}>
-          <h1 className="font-workSans font-title text-xl leading-6 mb-9">
+        <main style={{ height: "calc(100vh - 200px" }}>
+          <h1 className="font-workSans font-title text-xl leading-6 mt-2 mb-7">
             Digite seu endereço
           </h1>
-          <div className="flex flex-col justify-center gap-4">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-center gap-4"
+          >
             <Label labelHtmlFor="text">CEP</Label>
-            <Input placeholder="Digite aqui seu CEP" />
+            <Input
+              placeholder="Digite aqui seu CEP"
+              onChange={handleChangeInput}
+              name="cep"
+              value={formValues.cep}
+            />
             <Label labelHtmlFor="text">Endereço</Label>
-            <Input placeholder="Digite aqui o seu endereço" />
+            <Input
+              placeholder="Digite aqui o seu endereço"
+              onChange={handleChangeInput}
+              name="endereco"
+              value={formValues.endereco}
+            />
             <Label labelHtmlFor="text">Município</Label>
-            <Input placeholder="Digite aqui o nome da sua cidade" />
+            <Input
+              placeholder="Digite aqui o nome da sua cidade"
+              onChange={handleChangeInput}
+              name="municipio"
+              value={formValues.municipio}
+            />
             <Label labelHtmlFor="text">Estado</Label>
-            <Input placeholder="Digite aqui o Estado onde mora" />
-          </div>
+            <Input
+              placeholder="Digite aqui o Estado onde mora"
+              onChange={handleChangeInput}
+              name="estado"
+              value={formValues.estado}
+            />
+          </form>
         </main>
         <div className="flex flex-col justify-between">
           <Button typeButton="quinary">Continuar</Button>
