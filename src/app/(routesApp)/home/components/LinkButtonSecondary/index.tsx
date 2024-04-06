5d3afb402dac65e8f817a4c4efffd2d9285b62ab
yraffic02 <yraffic02@gmail.com>
2024-04-06 17:43:57 -0300

    feat: add novas images

--- src/app/(routesApp)/home/components/LinkButtonSecondary/index.tsx
+++ src/app/(routesApp)/home/components/LinkButtonSecondary/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 import { ReactNode } from "react"
 
@@ -16,10 +17,45 @@ export const LinkButtonSecondary = ({hRef, name}: LinkButtonTypeHome) =>{
                     bg-verdeFloresta 
                     rounded-tl-[1.25rem]
                     rounded-tr-[1.125rem]
-                    h-28    
-                    "
+                    h-36   
+                    relative
+                    flex"
             >
-                    
+                <Image 
+                    src='/illustrations/Window_3.svg'
+                    height={73}
+                    width={131}
+                    alt="imagem svg"
+                    className="absolute bottom-2 left-4"
+                />
+                <Image 
+                    src='/illustrations/Window_4.svg'
+                    height={78}
+                    width={102}
+                    alt="imagem svg"
+                    className="absolute top-1 left-28"
+                />
+                <Image 
+                    src='/illustrations/Window_1.svg'
+                    height={66}
+                    width={95}
+                    alt="imagem svg"
+                    className="absolute bottom-1 left-44"
+                />
+                <Image 
+                    src='/illustrations/Window_2.svg'
+                    height={45}
+                    width={47}
+                    alt="imagem svg"
+                    className="absolute top-3 right-10"
+                />
+                <Image 
+                    src='/illustrations/Plant.svg'
+                    height={70}
+                    width={34}
+                    alt="imagem svg"
+                    className="absolute bottom-0 right-2"
+                />
             </div>
             <span 
                 className="
