6c4fa8127ebfce34e999b8e8bfa55d4eef1c05c1
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-04-06 22:25:22 -0300

    feat: req in login page and retister page

--- src/app/opening/login/components/FormLogin/index.tsx
+++ src/app/opening/login/components/FormLogin/index.tsx
@@ -1,33 +1,52 @@
-import { Button } from "@/components/Button"
-import { Input } from "@/components/Input"
-import { Label } from "@/components/Label"
-import Link from "next/link"
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Label } from "@/components/Label";
+import axios from "axios";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export const FormLogin = () =>{
-    return(
-        <form className="w-full flex flex-col justify-center gap-3 form-mb">
+export const FormLogin = () => {
+    const [dataForm, setDataForm] = useState();
+    
 
-            <Label labelHtmlFor="email">Email</Label>
-            <Input id="email" placeholder="Digite aqui o seu Email" />
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("/user", dataForm);
+      console.log("Resposta do servidor:", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-            <Label labelHtmlFor="password">Senha</Label>
-            <Input id="password" placeholder="Digite sua senha" />
+  return (
+    <form
+      onSubmit={() => handleSubmit()}
+      className="w-full flex flex-col justify-center gap-3 form-mb"
+    >
+      <Label labelHtmlFor="email">Email</Label>
+      <Input id="email" placeholder="Digite aqui o seu Email" />
 
-            <div className="w-full flex justify-end items-end">
-            <Link href='#' className="text-body font-body text-verdeFloresta">
-                Esqueci minha senha
-            </Link>
-            </div>
+      <Label labelHtmlFor="password">Senha</Label>
+      <Input id="password" placeholder="Digite sua senha" />
 
-            <div className="w-full flex flex-col justify-center items-center gap-6 absolute bottom-0">
-            <Button typeButton="primary">
-                <p className="font-medium">Entrar</p>
-            </Button>
-            <span className="text-body font-body font-workSans leading-6 text-black">
-                Não tem conta ? <Link className="text-verdeFloresta cursor-pointer" href="/register">Cadastre-se</Link>
-            </span>
-            </div>
+      <div className="w-full flex justify-end items-end">
+        <Link href="#" className="text-body font-body text-verdeFloresta">
+          Esqueci minha senha
+        </Link>
+      </div>
 
-      </form>
-    )
-}
+      <div className="w-full flex flex-col justify-center items-center gap-6 absolute bottom-0">
+        <Button typeButton="primary">
+          <p className="font-medium">Entrar</p>
+        </Button>
+        <span className="text-body font-body font-workSans leading-6 text-black">
+          Não tem conta ?{" "}
+          <Link className="text-verdeFloresta cursor-pointer" href="/register">
+            Cadastre-se
+          </Link>
+        </span>
+      </div>
+    </form>
+  );
+};
