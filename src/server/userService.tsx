ef809fdba1d23910e8a7a0b3fd0b693ed98993cf
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-04-09 09:44:27 -0300

    Pull da dev

--- src/server/userService.tsx
+++ src/server/userService.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/"
-    //baseURL: "postgres://postgres.ssdabcxiuwvvwiypyzcz:reciclemais@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+    //baseURL: "http://localhost:3000/"
+    baseURL: "postgres://postgres.ssdabcxiuwvvwiypyzcz:reciclemais@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
 })
 
 export class UsuarioService{
