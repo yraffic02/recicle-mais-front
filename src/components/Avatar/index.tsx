0df9238e218d9505faabb154a67137e8ef5c3b63
unknown <novais-bruno@hotmail.com>
2024-04-09 10:36:54 -0300

    Frist commit

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,16 +1,6 @@
 "use client";
-import { getItem } from "@/utils/localStorageUtils";
-import { jwtDecode } from "jwt-decode";
 import Image from "next/image";
 
-interface IAvatarProps {
-    userId: number;
-    fullname: string;
-    email: string;
-    phone: string;
-    user_type: string;
-    avatar: string;
-}
 type avatarType = 'sm' | 'lg';
 
 interface IAvatar {
@@ -18,10 +8,6 @@ interface IAvatar {
 }
 
 export const Avatar = ({ typeAvatar }: IAvatar) => {
-
-    const token = getItem("token") as string;
-    const decoded: IAvatarProps = jwtDecode(token);
-    const avatar = decoded.avatar;
     return (
         <Image
             src={`/avatar/${avatar}`}
